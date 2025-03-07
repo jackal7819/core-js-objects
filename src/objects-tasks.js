1c6cb55ad0e500fd4bbc86d8f0511d554d17b2b8
jackal7819 <jackal3166@gmail.com>
2025-03-07 23:40:17 +0200

    feat: pass first 4 tests out of 20

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,8 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  return { ...obj };
 }
 
 /**
@@ -32,8 +32,16 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const map = new Map();
+
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      map.set(key, (map.get(key) || 0) + value);
+    });
+  });
+
+  return Object.fromEntries(map);
 }
 
 /**
